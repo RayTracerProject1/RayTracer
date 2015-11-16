b478a37e09fca7cca09bf8a9acb7c4e985537632
TheRealZeagan <jonaspepsimax@hotmail.com>
2015-11-16 12:52:02 +0100

    Kommentar addition
    Tilføjet kommentar

--- vector.h
+++ vector.h
@@ -1,26 +1,31 @@
+//vector addition product
 vector vectorAddition(vector *v1, vector *v2)
 {
         vector result = {v1 -> x + v2 -> x, v1 -> y + v2 -> y, v1 -> z + v2 -> z };
         return result;
 }
 
+//vector subtract product
 vector vectorSubtract(vector *v1, vector *v2)
 {
         vector result = {v1 -> x - v2 -> x, v1 -> y - v2 -> y, v1 -> z - v2 -> z };
         return result;
 }
 
-double vectorDotProduct(vector *v1, vector *v2)
+//vector dot product
+vector vectorDotProduct(vector *v1, vector *v2)
 {
         return v1 -> x * v2 -> x + v1 -> y * v2 -> y + v1 -> z * v2 -> z;
 }
 
+//vector scalar product
 vector vectorScalar(double a, vector *v)
 {
         vector result = {v -> x * c, v -> y * c, v -> z * c};
         return result;
 }
 
+//vector cross product
 vector vectorCrossProduct(vector *v1, vector *v2)
 {
 	vector result;
