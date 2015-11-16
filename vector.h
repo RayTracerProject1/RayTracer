f7ca8359f15a024fe1db04304b8f350e7d628712
antiknight <leo@edit.fo>
2015-11-16 23:46:24 +0100

    Saving changes, will start work on shading tomorrow

--- vector.h
+++ vector.h
@@ -1,38 +1,24 @@
+
+typedef struct {
+        double x;
+        double y;
+        double z;
+} vector;
+
+
+// Function Prototypes
+
 //vector addition product
-vector vectorAddition(vector *v1, vector *v2)
-{
-        vector result = {v1 -> x + v2 -> x, v1 -> y + v2 -> y, v1 -> z + v2 -> z };
-        return result;
-}
+vector vectorAddition(vector *v1, vector *v2);
 
 //vector subtract product
-vector vectorSubtract(vector *v1, vector *v2)
-{
-        vector result = {v1 -> x - v2 -> x, v1 -> y - v2 -> y, v1 -> z - v2 -> z };
-        return result;
-}
+vector vectorSubtract(vector *v1, vector *v2);
 
 //vector dot product
-vector vectorDotProduct(vector *v1, vector *v2)
-{
-        return v1 -> x * v2 -> x + v1 -> y * v2 -> y + v1 -> z * v2 -> z;
-}
+vector vectorDotProduct(vector *v1, vector *v2);
 
 //vector scalar product
-vector vectorScalar(double a, vector *v)
-{
-        vector result = {v -> x * c, v -> y * c, v -> z * c};
-        return result;
-}
+vector vectorScalar(double a, vector *v);
 
 //vector cross product
-vector vectorCrossProduct(vector *v1, vector *v2)
-{
-	vector result;
-
-	result.x = (v1 -> y * v2 -> z) - (v1 -> z * v2 -> y);
-	result.y = (v1 -> z * v2 -> x) - (v1 -> x * v2 -> z);
-	result.z = (v1 -> x * v2 -> y) - (v1 -> y * v2 -> x);
-
-	return result;
-}
+vector vectorCrossProduct(vector *v1, vector *v2);
