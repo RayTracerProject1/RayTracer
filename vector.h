1fdeacd48081ad489a99116a84cf470da3bbe375
TheRealZeagan <jonaspepsimax@hotmail.com>
2015-11-16 11:22:42 +0100

    Små ændringer

--- vector.h
+++ vector.h
@@ -1,23 +1,23 @@
 vector vectorAddition(vector *v1, vector *v2)
 {
-        vector result = {v1->x + v2->x, v1->y + v2->y, v1->z + v2->z };
+        vector result = {v1 -> x + v2 -> x, v1 -> y + v2 -> y, v1 -> z + v2 -> z };
         return result;
 }
 
 vector vectorSubtract(vector *v1, vector *v2)
 {
-        vector result = {v1->x - v2->x, v1->y - v2->y, v1->z - v2->z };
+        vector result = {v1 -> x - v2 -> x, v1 -> y - v2 -> y, v1 -> z - v2 -> z };
         return result;
 }
 
 double vectorDotProduct(vector *v1, vector *v2)
 {
-        return v1->x * v2->x + v1->y * v2->y + v1->z * v2->z;
+        return v1 -> x * v2 -> x + v1 -> y * v2 -> y + v1 -> z * v2 -> z;
 }
 
 vector vectorScalar(double a, vector *v)
 {
-        vector result = {v->x * c, v->y * c, v->z * c};
+        vector result = {v -> x * c, v -> y * c, v -> z * c};
         return result;
 }
 
@@ -25,9 +25,9 @@ vector vectorCrossProduct(vector *v1, vector *v2)
 {
 	vector result;
 
-	result.x = (v1->y * v2->z) - (v1->z * v2->y);
-	result.y = (v1->z * v2->x) - (v1->x * v2->z);
-	result.z = (v1->x * v2->y) - (v1->y * v2->x);
+	result.x = (v1 -> y * v2 -> z) - (v1 -> z * v2 -> y);
+	result.y = (v1 -> z * v2 -> x) - (v1 -> x * v2 -> z);
+	result.z = (v1 -> x * v2 -> y) - (v1 -> y * v2 -> x);
 
 	return result;
 }
