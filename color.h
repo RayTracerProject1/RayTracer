9a0cfae11684aeb1fbb5f5477c67adebe305eb8c
antiknight <leo@edit.fo>
2015-11-17 14:13:41 +0100

    Added color multiply with coefficient needed for shading. Moved structure for color type to color.h

---
+++ color.h
@@ -0,0 +1,12 @@
+
+
+typedef struct {
+	double red;
+	double green;
+	double blue;
+} color;
+
+// function prototype
+
+// multiply color with coefficient
+color colorCoeMul(double coef, color *c); 
