559179a28efd790e99b155a3797b9186884fdc93
TheRealZeagan <jonaspepsimax@hotmail.com>
2015-11-17 09:05:59 +0100

    Changes incomming

--- vector.h
+++ vector.h
@@ -8,6 +8,7 @@ typedef struct vector2
     float x, y, z, a;
 }vector2;
 
+//Vector 1 start:
 vector1 vectorAddition(vector1 *vec1, vector1 *vec2);
 
 vector1 vectorSubtract(vector1 *vec1, vector1 *vec2);
@@ -17,3 +18,4 @@ float vectorDotProduct(vector1 *vec1, vector1 *vec2);
 vector1 vectorScalar(float a, vector1 *v);
 
 vector1 vectorCrossProduct(vector1 *vec1, vector1 *vec2);
+//Vector 1 end:
