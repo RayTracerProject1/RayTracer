43180b088dd7a13ce150dbf509c709f6d42b12ef
TheRealZeagan <jonaspepsimax@hotmail.com>
2015-11-21 12:44:29 +0100

    Cleanup

--- dataStructure.h
+++ dataStructure.h
@@ -1,40 +1,17 @@
 #include "headers.h"
 
-//Brug vector headeren, den findes allerede -> vector.h
-/*typedef struct {
-	double x, y, z;
-} vector;
-* Also might want the structures for each ting in it's own header file,
-* so the struct for color will be in the color.h
-*/
-
-//test
-#define WOOD 0
-#define BRONZE 1
-#define SILVER 2
-#define GOLD 3
-#define PLATINUM 4
-#define DIAMOND 5
-#define TITANIUM 6
-
-//test
-typedef struct material
-{
-    int materialType;
-}material;
-
-typedef struct FOV
-{
-    double FOV;
-}FOV;
-
 typedef struct {
-	vector1 pos; // Starting position of ray
-	vector1 dir; // Direction of the ray from the starting position
+	Vector pos; // Starting position of ray
+	Vector dir; // Direction of the ray from the starting position
 } ray;
 
-typedef struct {
-	vector1 pos;
-	color lightColor;
-	double intensity;
-} light;
+typedef struct light
+{
+    Vector pos;
+    double intensity;
+}light;
+
+light lightMake(Vector pos, double intensity);
+Vector lightGetDirection(const light *light, Vector point);
+double lightGetDiffusedHighlight(const light *light, Vector lightDirection, Vector normal);
+double lightGetSpecularHighlight(const light *light, Vector lightDirection, Vector normal, Vector rayDirection, double specularity);
