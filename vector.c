417e1886c6f028240f91b2ef7eca17f6caec3b7a
TheRealZeagan <jonaspepsimax@hotmail.com>
2015-11-21 12:26:33 +0100

    Read the new Vectors

--- vector.c
+++ vector.c
@@ -1,44 +1,82 @@
 #include "headers.h"
 
-        /* Vector1 calculations start: */
-
-//vector addition product
-vector1 vectorAddition(vector1 *v1, vector1 *v2)
+Vector vecMake(double x, double y, double z)
 {
-        vector1 result = {v1 -> x + v2 -> x, v1 -> y + v2 -> y, v1 -> z + v2 -> z };
-        return result;
+    Vector v;
+    v.x = x, v.y = y, v.z = z;
+    return v;
 }
 
-//vector subtract product
-vector1 vectorSubtract(vector1 *v1, vector1 *v2)
+Vector vecAddition1(Vector a, Vector b)
 {
-        vector1 result = {v1 -> x - v2 -> x, v1 -> y - v2 -> y, v1 -> z - v2 -> z };
-        return result;
+    return vecMake(a.x + b.x, a.y + b.y, a.z + b.z);
 }
 
-//vector dot product
-vector1 vectorDotProduct(vector1 *v1, vector1 *v2)
+Vector vecAddition2(Vector a, Vector b, Vector c)
 {
-        vector1 result = {x * v2 -> x + v1 -> y * v2 -> y + v1 -> z * v2 -> z}:
-        return result;
+    return vecMake(a.x + b.x + c.x, a.y + b.y + c.y, a.z + b.z + c.z);
 }
 
-//vector scalar product
-vector1 vectorScalar(double a, vector1 *v)
+double  vecDotProduct(Vector a, Vector b)
 {
-        vector1 result = {v -> x * c, v -> y * c, v -> z * c};
-        return result;
+    return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
 }
 
-//vector cross product
-vector1 vectorCrossProduct(vector1 *v1, vector1 *v2)
+Vector vecSubtract(Vector a, Vector b)
 {
-	vector1 result;
-
-	result.x = (v1 -> y * v2 -> z) - (v1 -> z * v2 -> y);
-	result.y = (v1 -> z * v2 -> x) - (v1 -> x * v2 -> z);
-	result.z = (v1 -> x * v2 -> y) - (v1 -> y * v2 -> x);
-
-	return result;
+    return vecMake(a.x - b.x, a.y - b.y, a.z - b.z);
 }
-        /* Vector1 calculations end: */
+
+Vector vecMultiplyByVec(Vector a, Vector b)
+{
+    return vecMake(a.x * b.x, a.y * b.y, a.z * b.z);
+}
+
+Vector vecMultiply(Vector v, double b)
+{
+    return vecMake(v.x * b, v.y * b, v.z * b);
+}
+
+Vector vecDivide(Vector v, double b)
+{
+    return vecMultiply(v, 1.0 / b);
+}
+
+double vecLength(Vector v)
+{
+    return sqrt(SQUARE(v.x) + SQUARE(v.y) + SQUARE(v.z));
+}
+
+//Copy Pasta. Needed this for units.
+static double invSqrt1(double y)
+{
+    double xhalf = ( double )0.5 * y;
+    long long i = *( long long* )( &y );
+
+//LL suffix for (long long) type for GCC
+    i = 0x5fe6ec85e7de30daLL - ( i >> 1 );
+    y = *( double* )( &i );
+    y = y * ( ( double )1.5 - xhalf * y * y );
+
+    return y;
+}
+
+Vector vecUnit(Vector v)
+{
+    double is = invSqrt1(SQUARE(v.x) + SQUARE(v.y) + SQUARE(v.z));
+    return vecMultiply()ult(v, is);
+}
+
+Vector vecCrossProduct(Vector a, Vector b)
+{
+    Vector v;
+    v.x = (a.y * b.z) - (b.y * a.z);
+    v.y = (a.z * b.x) - (b.z * a.x);
+    v.z = (a.x * b.y) - (b.x * a.y);
+    return v;
+}
+
+Vector vecNegate(Vector v)
+{
+    return vecMake(-v.x, -v.y, -v.z);
+}
--- vector.h
+++ vector.h
@@ -1,21 +1,68 @@
-typedef struct vector1
+//Define zero point of the vector.
+#define VECTOR_ZERO vec_make(0.0, 0.0, 0.0)
+
+//Define addition calculation           This for seperation
+#define VECTOR_ADDITION1(out, a, b)          \
+                (out).x = (a).x + (b).x;     \
+                (out).y = (a).y + (b).y;     \
+                (out).z = (a).z + (b).z;
+//Define addition calculation               This for seperation
+#define VECTOR_ADDITION2(out, a, b, c)              \
+                (out).x = (a).x + (b).x + (c).x;    \
+                (out).y = (a).y + (b).y + (c).x;    \
+                (out).z = (a).z + (b).z + (c).x;
+
+//Define subtraction calculation         This for seperation
+#define VECTOR_SUBTRACTION(out, a, b)       \
+                (out).x = (a).x - (b).x;    \
+                (out).y = (a).y - (b).y;    \
+                (out).z = (a).z - (b).z;
+
+//Define dot product calculation        This for seperation
+#define VECTOR_DOTPRODUCT(a, b)              \
+                ((a).x * (b).x + (a).y * (b).y + (a).z * (b).z)
+
+//Define cross product calculation                 This for seperation
+#define VECTOR_CROSSPRODUCT(out, a, b)                     \
+                (out).x=((a).y * (b).z) - ((b).y * (a).z); \
+                (out).y=((a).z * (b).x) - ((b).z * (a).x); \
+                (out).z=((a).x * (b).y) - ((b).x * (a).y);
+
+//Define multiply calculation      This for seperation
+#define VECTOR_MULTIPLYVEC(out, a, b)   \
+                (out).x = (a).x * b;    \
+                (out).y = (a).y * b;    \
+                (out).z = (a).z * b
+
+//Define multiply calculation             this for seperation
+#define VECTOR_MULTIPLY2(out, a, b)          \
+                (out).x = (a).x * (b).x;     \
+                (out).y = (a).y * (b).y;     \
+                (out).z = (a).z * (b).z;
+
+//Define divide calculation         This for seperation
+#define VECTOR_DIVIDE(out, a, b)      \
+                (out).x = (a).x / b;  \
+                (out).y = (a).y / b;  \
+                (out).z = (a).z / b;
+
+//Make vector with x, y and z axis
+typedef struct Vector
 {
     double x, y, z;
-}vector1;
+}Vector;
 
-typedef struct vector2
-{
-    double x, y, z, a;
-}vector2;
+//Use Vector for all calculations
+Vector vecMake(double x, double y, double z);
+Vector vecAddition1(Vector a, Vector b);
+Vector vecAddition2(Vector a, Vector b, Vector c);
+double vecDotProduct(Vector a, Vector b);
+Vector vecSubtract(Vector a, Vector b);
+Vector vecMultiplyByVec(Vector a, Vector b);
+Vector vecMultiply(Vector v, double b);
+Vector vecDivide(Vector v, double b);
+double vecLength(Vector v);
+Vector vecUnit(Vector v);
+Vector vecCrossProduct(Vector a, Vector b);
+Vector vecNegate(Vector v);
 
-//Vector 1 start:
-vector1 vectorAddition(vector1 *v1, vector1 *v2);
-
-vector1 vectorSubtract(vector1 *v1, vector1 *v2);
-
-double vectorDotProduct(vector1 *v1, vector1 *v2);
-
-vector1 vectorScalar(double a, vector1 *v);
-
-vector1 vectorCrossProduct(vector1 *v1, vector1 *v2);
-//Vector 1 end:
